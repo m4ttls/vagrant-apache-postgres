2349a6d5e5689f44effd7d6d910c8e86140909f7
Giorgio Fellipe <giorgiofellipe@gmail.com>
2015-03-23 11:16:04 -0300

    Changed default install config from mysql to postgres database

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,20 +15,41 @@ sudo apt-get -y upgrade
 sudo apt-get install -y apache2
 sudo apt-get install -y php5
 
+# postgres
+sudo apt-get install -y postgresql postgresql-contrib
+
+POSTGRE_VERSION=9.3
+
+# listen for localhost connections
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/$POSTGRE_VERSION/main/postgresql.conf
+
+# identify users via "md5", rather than "ident", allowing us to make postgres
+# users separate from system users. "md5" lets us simply use a password
+echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /etc/postgresql/$POSTGRE_VERSION/main/pg_hba.conf
+sudo service postgresql start
+
+# create new user "root" with defined password "root" not a superuser
+sudo -u postgres psql -c "CREATE ROLE root LOGIN UNENCRYPTED PASSWORD 'root' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
+
+# create new database "database"
+sudo -u postgres psql -c "CREATE DATABASE database"
+
+sudo service postgresql restart
+
 # install mysql and give password to installer
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server
-sudo apt-get install php5-mysql
+#sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+#sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+#sudo apt-get -y install mysql-server
+#sudo apt-get install php5-mysql
 
 # install phpmyadmin and give password(s) to installer
 # for simplicity I'm using the same password for mysql and phpmyadmin
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get -y install phpmyadmin
+#sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+#sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+#sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+#sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+#sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+#sudo apt-get -y install phpmyadmin
 
 # setup hosts file
 VHOST=$(cat <<EOF
